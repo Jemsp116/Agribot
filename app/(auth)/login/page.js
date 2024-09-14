88b1b7285faf6a2549d9fc51c39edbdece37d5fc
Jemsp <jemspatel1080@gmail.com>
2024-09-14 19:17:24 +0530

    :money_with_wings:

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -5,7 +5,7 @@ import React from 'react'
 const LoginPage = () => {
   return (
     <div>
-        <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Login to E-WasteMart'}>
+        <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Login to Agribot'}>
             <LoginForm />
         </DivWrapper>
     </div>
