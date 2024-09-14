95c6bfb8a68c68986dcfcd23f5ce6595c346ad47
Jemsp <jemspatel1080@gmail.com>
2024-09-14 22:28:28 +0530

    :money_with_wings: login and register

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -5,13 +5,9 @@ import React from 'react'
 const LoginPage = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Login to AgriBot'}>
-=======
-        <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Login to Agribot'}>
->>>>>>> 88b1b7285faf6a2549d9fc51c39edbdece37d5fc
-            <LoginForm />
-        </DivWrapper>
+      <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Login to Agribot'}>
+        <LoginForm />
+      </DivWrapper>
     </div>
   )
 }
--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -5,14 +5,10 @@ import React from 'react'
 const RegisterPage = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Welcome to AgriBot'}>
-=======
-        <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Welcome to Agribot'}>
->>>>>>> 88b1b7285faf6a2549d9fc51c39edbdece37d5fc
-            <p>Fill this form to register.</p>
-            <RegisterForm />
-        </DivWrapper>
+      <DivWrapper className={'border py-10 max-w-[600px] mx-auto shadow-md rounded-lg'} title={'Welcome to Agribot'}>
+      <p>Fill this form to register.</p>
+      <RegisterForm />
+    </DivWrapper>
     </div>
   )
 }
